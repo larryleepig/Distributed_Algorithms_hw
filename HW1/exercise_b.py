68ef9115858452734a9f60efd8cdb08ef2a2dd62
李仲翊 <cs102.larryleepig@ir.nctu.edu.tw>
2018-12-01 22:31:01 +0800

    final  version

--- HW1/exercise_b.py
+++ HW1/exercise_b.py
@@ -5,33 +5,63 @@ f = open(sys.argv[1], "r")
 n = int(f.readline()) #n node
 
 nodes = f.readline()
-nodeWeight = {}
 
-adjMatrix = []
+  
+neighbor = []
 for i in range(n):
     tmp = []
     line = f.readline()
     for j in line.split():
         tmp.append(int(j))
-    adjMatrix.append(tmp)
+    neighbor.append(tmp)
+
+weight = []
+oriWeight = []  
+for i, w in enumerate(nodes.split()):
+    weight.append(int(w)/sum(neighbor[i]))
+    oriWeight.append(int(w))
 f.close()
 
-for i, weight in enumerate(nodes.split()):
-    weight = int(weight)/sum(adjMatrix[i])
-    nodeWeight[i] = weight
-answer = []
+newMwis = []
+for i in range(n):
+    newMwis.append(0) #init everyone is not in Mwisn
 
-while len(nodeWeight) > 0:
-    temp = sorted(nodeWeight.items(), key=lambda d: d[0], reverse = True) #Let large id to sorted last
-    temp = sorted(temp, key = lambda d: d[1])
-    ansTemp = temp.pop()
-    del nodeWeight[ansTemp[0]]
-    for i, connect in enumerate(adjMatrix[ansTemp[0]]):
-        if connect == 1 and nodeWeight.get(i) != None:
-            del nodeWeight[i]
-    answer.append(ansTemp[0]) 
-    
-#print(adjMatrix)
-print("MWIS : ",end = '')
+round = 0
+while True: # to do round
+    oldMwis = []
+    for i in range(n):
+        oldMwis.append(newMwis[i])
+    for i in range(n):
+        for j, connect in enumerate(neighbor[i]):
+            if connect == 1:
+                if oldMwis[j] == 1:
+                    if weight[j] > weight[i]:
+                        newMwis[i] = 0
+                        break
+                    elif weight[j] == weight[i] and j < i:
+                        newMwis[i] = 0
+                        break
+                    else:
+                        newMwis[i] = 1
+                else:
+                    newMwis[i] = 1
+    finish = True
+    round += 1
+    for i in range(n):
+        if oldMwis[i] != newMwis[i]:
+            finish = False
+            break
+    if finish:
+        break
+
+answer = []
+total = 0
+for i, b in enumerate(newMwis):
+    if b == 1:
+        answer.append(i)
+        total += oriWeight[i]
+
+print("MWIS : ", end='')
 print(sorted(answer))
+print("Total weight: ",total)
 
